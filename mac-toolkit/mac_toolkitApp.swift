cb5c1e1b2298aeafb9a29a4b7e4cfa05892292ad
leonw111 <leigang@anz.io>
2026-01-31 12:36:47 +0800

    Initial Commit

---
+++ mac-toolkit/mac_toolkitApp.swift
@@ -0,0 +1,17 @@
+//
+//  mac_toolkitApp.swift
+//  mac-toolkit
+//
+//  Created by leon on 2026/1/31.
+//
+
+import SwiftUI
+
+@main
+struct mac_toolkitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
